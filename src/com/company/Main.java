51698794283407155e08d32ee872e6f94f6c5f45
ivand <ivandariorap1@outlook.es>
2022-07-07 20:33:15 -0500

    bucle while

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numeroWhile = 0;
+
+        while(numeroWhile <3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+
+}
